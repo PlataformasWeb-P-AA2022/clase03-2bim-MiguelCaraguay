3961481dbf9a8c7019af7679d09322cd9cd04631
MiguelCaraguay <miguelcaraguay.1502@gmail.com>
2022-06-20 14:51:08 -0500

    clase03

--- ejemplo1/proyectoUno/administrativo/urls.py
+++ ejemplo1/proyectoUno/administrativo/urls.py
@@ -9,8 +9,8 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
-        path('listado-estudiantes', views.listadoEstudiantes, 
+        path('listado/estudiantes', views.listadoEstudiantes, 
             name='listadoEstudiates'),
-        path('listado-estudiantes-dos', views.listadoEstudiantesDos, 
+        path('listado/estudiantes/dos', views.listadoEstudiantesDos, 
             name='listadoEstudiatesDos'),
- ]
+]
